b8445e6a5279f13b2ed7adfaaa330bc7bb95d66f
constyejin <lightup.jin@gmail.com>
2023-06-13 10:41:16 +0900

    📝 4ways to import a module in python

--- 230613/test_module.py
+++ 230613/test_module.py
@@ -1,6 +1,6 @@
-# import class_basic
-# obj1 = class_basic.FourCal3()
-# print("용병전사", obj1.add(5, 2), obj1.subst(5, 2))
+import class_basic
+obj1 = class_basic.FourCal3()
+print("용병전사", obj1.add(5, 2), obj1.subst(5, 2))
 
 
 # import class_basic as cb
@@ -11,6 +11,6 @@
 # obj3 = FourCal3()
 # print(obj3.add(5, 2))
 
-from class_basic import FourCal3 as fc3
-obj4 = fc3()
-print(obj4.add(5, 2))
+# from class_basic import FourCal3 as fc3
+# obj4 = fc3()
+# print(obj4.add(5, 2))
