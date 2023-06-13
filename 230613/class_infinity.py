91cad92f2807817c0ccd2edfc8f5c8da5e9e86ae
constyejin <lightup.jin@gmail.com>
2023-06-13 11:09:21 +0900

    ✨ class 상속 (기존 plus, minus & add mul, div)

---
+++ 230613/class_infinity.py
@@ -0,0 +1,16 @@
+from class_basic import FourCal3 as fc3
+
+class Cbup(fc3):
+  def mul(self, num1, num2):
+    result = num1 * num2
+    return result
+
+  def div(self, num1, num2):
+    if num2 != 0 :
+      result = num1 / num2
+    else :
+      result = "ERROR!"
+    return result
+
+obj1 = Cbup()
+print(obj1.add(5, 5), obj1.subst(5, 5), obj1.mul(5, 5), obj1.div(5, 5))
