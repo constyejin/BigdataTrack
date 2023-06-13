4feaf86469926fc239911943df2e7d1748e0eb0a
constyejin <lightup.jin@gmail.com>
2023-06-13 11:19:03 +0900

    ✨ class 재정의 (\n, \t)

--- 230613/class_infinity.py
+++ 230613/class_infinity.py
@@ -14,3 +14,25 @@ class Cbup(fc3):
 
 obj1 = Cbup()
 print("전직전사", obj1.add(5, 5), obj1.subst(5, 5), obj1.mul(5, 5), obj1.div(5, 5))
+
+
+# 클래스 재정의
+class cbupup(fc3):
+  def add(self, num1, num2):
+    result = num1 + num2
+    return "더하기 결과는 " + str(result) + "입니다.\n"
+    # result_text = "더하기 결과는 {} 입니다".format(str(result))
+
+  def mul(self, num1, num2):
+    result = num1 + num2
+    return "곱하기 결과는 " + str(result) + "입니다.\n"
+
+  def div(self, num1, num2):
+    if num2 != 0:
+      result = num1 / num2
+    else :
+      result = "ERROR!"
+    return "나누기 결과는 " + str(result) + "입니다."
+
+obj3 = cbupup()
+print("2차 전직전사\n", obj3.add(1, 1), obj3.mul(1, 1), obj3.div(1, 1))
