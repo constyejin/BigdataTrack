e9eb71f22bbb60dd1dc300034e2fd38e161cb68c
constyejin <lightup.jin@gmail.com>
2023-06-13 10:10:55 +0900

    ✨ class 만들기 : setdata

---
+++ 230613/class_basic.py
@@ -0,0 +1,16 @@
+class FourCal1:
+  def setdata(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+
+  def add(self):
+    result = self.num1 + self.num2
+    return result
+
+  def subst(self):
+    result = self.num1 - self.num2
+    return result
+
+obj1 = FourCal1()
+obj1.setdata(5,2)
+print("1번", obj1.add())
