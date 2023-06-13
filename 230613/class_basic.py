c050868f8e935a4d13944cb9f86b4ca0d9b4a2a1
constyejin <lightup.jin@gmail.com>
2023-06-13 10:23:24 +0900

    ✨ make calss method 3

--- 230613/class_basic.py
+++ 230613/class_basic.py
@@ -1,3 +1,4 @@
+# 1
 class FourCal1:
   def setdata(self, num1, num2):
     self.num1 = num1
@@ -20,6 +21,7 @@ obj2.setdata(2,2)
 print("마법사1", obj2.add(), obj2.subst())
 
 
+# 2
 class FourCal2:
   # __init__ 사용시 setdata 없이 사용 가능
   def __init__(self, num1, num2):
@@ -36,3 +38,21 @@ class FourCal2:
 
 obj2 = FourCal2(10, 5)
 print("전사2", obj2.add(), obj2.subst())
+
+
+# 3 method로 바로 데이터를 받는다
+class FourCal3:
+  def __init__(self):
+    print("FourCal3를 이용해 주셔서 감사합니다.")
+
+  def add(self, num1, num2):
+    result = num1 + num2
+    return result
+
+  def subst(self, num1, num2):
+    result = num1 - num2
+    return result
+
+obj3 = FourCal3()
+
+print("전사3", obj3.add(10, 2), obj3.subst(10, 2))
