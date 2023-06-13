3b7d0da86f588ee26b387da6bfc1a59175f00fcb
constyejin <lightup.jin@gmail.com>
2023-06-13 10:39:34 +0900

    ✨ from class_basic import FourCal3

--- 230613/test_module.py
+++ 230613/test_module.py
@@ -3,6 +3,10 @@
 # print("용병전사", obj1.add(5, 2), obj1.subst(5, 2))
 
 
-import class_basic as cb
-obj2 = cb.FourCal3()
-print(obj2.add(5, 2))
+# import class_basic as cb
+# obj2 = cb.FourCal3()
+# print(obj2.add(5, 2))
+
+from class_basic import FourCal3
+obj3 = FourCal3()
+print(obj3.add(5, 2))
