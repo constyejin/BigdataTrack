483a3e59958f10d75ac682893bb5ed5f92f4a2f5
constyejin <lightup.jin@gmail.com>
2023-06-13 16:57:23 +0900

    ✨ 네이버 뉴스 크롤링 (keyword : 빅데이터)

---
+++ 230613/news_crolling.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EB%B9%85%EB%8D%B0%EC%9D%B4%ED%84%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=25&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=21"
+response = requests.get(url)
+
+response.text
+soup = BeautifulSoup(response.text, "html.parser")
+
+title_list = []
+links = soup.find_all("a", class_="news_tit")
+
+# print(type(links))
+# print(links)
+
+# enumerate : 일련 번호 자동 생성 후 할당
+for i, link in enumerate(links):
+  title = link.text
+  url = link["href"]
+  title_list.append([i, title, url])
+  print(i, title, url)
