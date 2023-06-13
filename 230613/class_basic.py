8d1bd3bcf091145588872d7ed261a225607c5118
constyejin <lightup.jin@gmail.com>
2023-06-13 10:17:45 +0900

    ✨ Created class FourCal2

--- 230613/class_basic.py
+++ 230613/class_basic.py
@@ -18,3 +18,21 @@ print("전사", obj1.add(), obj1.subst())
 obj2 = FourCal1()
 obj2.setdata(2,2)
 print("마법사", obj2.add(), obj2.subst())
+
+
+class FourCal2:
+  # __init__ 사용시 setdata 없이 사용 가능
+  def __init__(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+
+  def add(self):
+    result = self.num1 + self.num2
+    return result
+
+  def subst(self):
+    result = self.num1 - self.num2
+    return result
+
+obj2 = FourCal2(10, 5)
+print("2번", obj2.add(), obj2.subst())
