e3cf4fce13770af8df5d46ff46f0c588c6fe7cb0
constyejin <lightup.jin@gmail.com>
2023-06-14 13:55:14 +0900

    ✨ naver api image

---
+++ 230614/naverapi_img.py
@@ -0,0 +1,28 @@
+# from requests import get
+import requests
+import time
+import os
+import json
+
+client_id = "O0sl9zVJg2m8bw809A3v"
+client_pw = "WAaH_SJDwf"
+query = "청바지"
+
+# 이미지 수집하는 메인 함수
+def get_list(query):
+  print(query)
+  user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+  headers = {"User-Agent": user_agent, 
+             "X-Naver-Client-Id": client_id, 
+             "X-Naver-Client-Secret": client_pw
+             }
+
+  url = "https://openapi.naver.com/v1/search/image?query=" + query
+  print(url)
+
+  response = requests.get(url, headers=headers)
+  print(response.text)
+  elements = json.loads(response.text)['items']
+
+
+get_list(query)
