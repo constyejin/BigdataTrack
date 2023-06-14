f56bafcb9b83a3188987c18a187307ea92b32b17
constyejin <lightup.jin@gmail.com>
2023-06-14 14:33:52 +0900

    ✨ Created images folder

--- 230614/naverapi_img.py
+++ 230614/naverapi_img.py
@@ -4,17 +4,28 @@ import requests
 import time
 import os
 import json
+import pandas as pd
 
 client_id = "O0sl9zVJg2m8bw809A3v"
 client_pw = "WAaH_SJDwf"
 query = "청바지"
 
+def get_img(i, url):
+  user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+  headers = {"User-Agent": user_agent}
+
+  # 파일 확장자명
+  file_ext = url.split(".")[-1]
+  img = requests.get(url).content
+  open('/images/+'+str(i)+"."+file_ext,'wb').write(img)
+
+
 # 이미지 수집하는 메인 함수
 def get_list(query):
   print(query)
   user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-  headers = {"User-Agent": user_agent, 
-             "X-Naver-Client-Id": client_id, 
+  headers = {"User-Agent": user_agent,
+             "X-Naver-Client-Id": client_id,
              "X-Naver-Client-Secret": client_pw
              }
 
@@ -25,5 +36,24 @@ def get_list(query):
   print(response.text)
   elements = json.loads(response.text)['items']
 
+  for i, elm in enumerate(elements):
+    title = elm['title']
+    link = elm['link']
+    thumbnail = elm['thumbnail']
+    sizewidth = elm['sizewidth']
+    sizeheight = elm['sizeheight']
+    img_list.append([title, link, thumbnail, sizewidth, sizeheight])
+    print([title, link, thumbnail, sizewidth, sizeheight])
+
+    get_img(i, link)
+
+  return img_list
+
+img_list = []
+img_list =  get_list(query)
+
+
+df = pd.DataFrame(img_list, columns=['title', 'link', 'thumbnail', 'sizewidth', 'sizeheight'])
+df.to_csv('img_list.csv', index=False)
 
 get_list(query)
