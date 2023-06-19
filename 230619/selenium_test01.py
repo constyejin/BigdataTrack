3e031e5b55ff2ae37edd7516848570a044a5da1b
constyejin <lightup.jin@gmail.com>
2023-06-19 09:57:29 +0900

    ✨ from selenium.webdriver.common.by import By

--- 230619/selenium_test01.py
+++ 230619/selenium_test01.py
@@ -1,5 +1,7 @@
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
+
 
 driver = webdriver.Chrome()
 
@@ -7,8 +9,12 @@ url = "https://news.naver.com"
 driver.switch_to.window(driver.window_handles[0])
 driver.get(url)
 
+driver.find_element(By.CLASS_NAME, "Nicon_search").click()
 time.sleep(1)
 
+driver.find_element(By.CSS_SELECTOR, "input[type='search']").send_keys("빅데이터")
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+time.sleep(1)
 
 while(True):
     pass
