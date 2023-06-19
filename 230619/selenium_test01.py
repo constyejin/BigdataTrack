6ffb78d9396124bd37ce2b91453910f4db26313f
constyejin <lightup.jin@gmail.com>
2023-06-19 10:33:25 +0900

    ✨ driver.window_handles[1] 최신순 선택

--- 230619/selenium_test01.py
+++ 230619/selenium_test01.py
@@ -16,5 +16,21 @@ driver.find_element(By.CSS_SELECTOR, "input[type='search']").send_keys("빅데�
 driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 time.sleep(1)
 
+# New tap
+driver.switch_to.window(driver.window_handles[1])
+time.sleep(1)
+driver.find_elements(By.CSS_SELECTOR, "a[class='item']")[1].click()
+
+bodies = driver.find_elements(By.CSS_SELECTOR, "li[class='bx']")
+
+news_list = []
+for body in bodies:
+    title = body.find_element(By.CSS_SELECTOR, "a[class='news_tit']").text
+    link = body.find_element(By.CSS_SELECTOR, "a[class='news_tit']").get_attribute("href")
+    news_list.append([title, link])
+
+print(news_list)
+
+
 while(True):
     pass
